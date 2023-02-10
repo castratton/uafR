aecde6101d5730508d489cce1ff3ce39d71f62bd
Chase Stratton <stratton@landinstitute.org>
2023-02-10 14:09:23 -0600

    External standard data etc.

---
+++ R/ExternalStandard_data.R
@@ -0,0 +1,15 @@
+#'Sample Data Set for External Standards
+#'
+#'A mock data set to show the correct format for
+#'the `ES_calibration` input when using `standardifyIt()`
+#'with an External standard
+#'#'
+#'@format ## `ExternalStandard_data`
+#'A data frame with 28 rows and 4 columns:
+#'\describe{
+#'  \item{Quantity}{Measured Amount of Analyte in Sample}
+#'  \item{Component.Area}{Area/Quantity of Analyte}
+#'  \item{Component.RT}{Retention Time of Analyte}
+#'  \item{Replicate}{Experimental Replicate}
+#'  }
+"ExternalStandard_data"
