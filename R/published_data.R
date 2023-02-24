cdf131f5dbb2cc6bbe966144f0786bc2a4e315c0
Chase Stratton <stratton@landinstitute.org>
2023-02-24 17:51:06 -0600

    Adding data sets for publishing

---
+++ R/published_data.R
@@ -0,0 +1,16 @@
+#'Supplemental data set for Tables 1 and 2 in publication
+#'
+#'A data frame containing raw output from Agilent's Unknowns Analysis
+#'
+#'@format
+#'A data frame with 2433 rows and 7 columns:
+#'\describe{
+#'  \item{Component.RT}{Retention time of captured component}
+#'  \item{Base.Peak.MZ}{Mass to charge ratio (m/z) of captured component}
+#'  \item{Component.Area}{Area quantified for captured component}
+#'  \item{Compound.Name}{Tentative compound identity for captured component}
+#'  \item{Match.Factor}{Library match factor for captured component (i.e., accuracy of identification)}
+#'  \item{Sample.Name}{Name of sample from which component was captured}
+#'  \item{File.Name}{Name of file from which component was captured}
+#'  }
+"published_data"
---
+++ R/standards_model_data.R
@@ -0,0 +1,16 @@
+#'Supplemental data set for Figure 1 in publication
+#'
+#'Contains summary data that was used for linear model figures/analyses of standards data
+#'
+#'@format
+#'A data frame with 16 rows and 7 columns:
+#'\describe{
+#'  \item{uafR.RT}{Retention Time identified by uafR for referenced chemical}
+#'  \item{uafR.Mass}{Exact Mass found by uafR for referenced chemical}
+#'  \item{PubChem.Mass}{Mass shown on PubChem web interface for referenced chemical}
+#'  \item{Chemical}{Name of chemical}
+#'  \item{Sample}{Sample from which the chemical was identified}
+#'  \item{ng.Injected}{Amount of standard chemical injected into sample}
+#'  \item{uafR.Peak.Area}{Total component area identified by uafR for referenced chemical}
+#'  }
+"standards_model_data"
